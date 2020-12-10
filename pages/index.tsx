5d879dae9d154af3c6f3bdb6289a1a0d3fb884a2
Jacky-Summer <1106619871@qq.com>
2020-12-10 21:42:51 +0800

    style: 🎨 use prettier to format code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div>Hello nextjs-ts-redux-antd-starter</div>
-  )
+  return <div>Hello nextjs-ts-redux-antd-starter</div>
 }
 
 export default Home
