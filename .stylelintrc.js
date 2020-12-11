cc19029876ce9358b3154b87b82dcf10f38dbc05
Jacky-Summer <1106619871@qq.com>
2020-12-11 09:02:50 +0800

    style: 🎨 add stylelint to format css file

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-prettier',
+  ],
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
+}
