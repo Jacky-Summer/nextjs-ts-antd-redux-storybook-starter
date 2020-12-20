a4f24008cf878beeba66ea258489c4c30f99ad9d
Jacky-Summer <1106619871@qq.com>
2020-12-20 22:07:02 +0800

    test: ✅ write Footer stories

---
+++ src/components/Footer/index.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from '.'
+
+export default {
+  title: 'Example/Footer',
+  component: Footer,
+}
+
+const Template = () => <Footer />
+
+export const Default = Template.bind({})
